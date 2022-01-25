7902832af4e24ca629ebdeb36968e629ad7f7d0e
Fausto Lopez <flopez@baosystems.com>
2022-01-25 15:37:17 -0500

    Add files
    Added files to repo.

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Clone this repo
+2. Create an .Rprofile
+3. Run the app and reference the code
+
+More instructions coming soon...
---
+++ example_Rprofile.R
@@ -0,0 +1,2 @@
+# set your .Rprofile before starting the app and point it to your server
+Sys.setenv(BASE_URL = "https://test.datim.org/")
